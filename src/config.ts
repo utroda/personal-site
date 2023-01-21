eb2292e336f6dbad601ab6a278c1953e4ef9a184
utroda <utroda@MacBook-Air.localdomain>
2023-01-21 04:59:50 -0800

    fixing description

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jared West, Front End Developer Blog';
+export const SITE_DESCRIPTION = 'Front end developer blog talking about design systems and vuejs';
