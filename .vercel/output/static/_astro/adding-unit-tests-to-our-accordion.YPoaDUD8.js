e9e1dad3884a2da16308cf52ffcec3b69e169ad1
utroda <1255024+utroda@users.noreply.github.com>
2024-02-23 21:52:15 -0800

    updated astro and new theme

---
+++ .vercel/output/static/_astro/adding-unit-tests-to-our-accordion.YPoaDUD8.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./astro/assets-service.DcMRmNXG.js";async function o(){return t(()=>import("./adding-unit-tests-to-our-accordion.Bl0CMd4y.js"),__vite__mapDeps([0,1,2,3,4]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["_astro/adding-unit-tests-to-our-accordion.Bl0CMd4y.js","_astro/_astro_assets.kWUfgQAZ.js","_astro/astro/assets-service.DcMRmNXG.js","_astro/hoisted.DlncgKtM.js","_astro/render-template.DUaq-Qx8.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ .vercel/output/static/_astro/setting-up-docker-node-vue-vite.D5OTTta_.js
@@ -0,0 +1,21 @@
+const e="setting-up-docker-node-vue-vite.md",n="post",t="setting-up-docker-node-vue-vite",o=`
+We will be working out of a Mono Repo using NPM.
+
+Creating folder for vue/node - create docker compose file, create nginx file
+
+Adding content to docker compose file
+
+Adding content to nginx file
+
+Updating vite.config.js, maintains HMR
+
+Using Fastify and setting the correct port.
+
+Run Docker Compose Up
+
+`,i={title:"Setting up a dev enviorment with Docker, Nginx, Node and Vue",description:"We will be setting up a development enviorment with Docker using Docker Compose, Nginx, Node and Vue.",publishDate:new Date(170928e7),draft:!0,tags:["node","vue","docker","nginx"]},d={type:"content",filePath:"/Users/utroda/github/personal-site/src/content/post/setting-up-docker-node-vue-vite.md",rawData:`
+title: "Setting up a dev enviorment with Docker, Nginx, Node and Vue"
+description: "We will be setting up a development enviorment with Docker using Docker Compose, Nginx, Node and Vue."
+publishDate: "01 Mar 2024"
+tags: ["Node", "Vue", "Docker", "nginx"]
+draft: true`};export{d as _internal,o as body,n as collection,i as data,e as id,t as slug};
